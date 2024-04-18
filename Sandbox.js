b5026093ca42947678ecccac0d7a7117ebcba987
Fahl <2elliot.fahl@gulllakecs.org>
2024-04-18 13:30:02 -0400

    Initial Sandbox Commit

---
+++ Sandbox.js
@@ -0,0 +1,10 @@
+// Programmer: Elliot Fahl
+// Team Member: Ezinna Nwokenta
+// Bolt ID:
+// Date: 4.16.2024
+// Program: Hello World - Sandbox
+// Program URL: https://edu.sphero.com/program/16700422/edit
+
+async function startProgram() {
+	// Write code here
+}
