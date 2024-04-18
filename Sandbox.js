0d41071c9cdf8cf3f4523f3f1bfa920498ec5687
Fahl <2elliot.fahl@gulllakecs.org>
2024-04-18 14:16:46 -0400

    Added Hello World script from wiki

--- Sandbox.js
+++ Sandbox.js
@@ -5,6 +5,11 @@
 // Program: Hello World - Sandbox
 // Program URL: https://edu.sphero.com/program/16700422/edit
 
+
 async function startProgram() {
-	// Write code here
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
 }
