34e370f6009145426900065b5b99e1b1863f5f33
Fahl <2elliot.fahl@gulllakecs.org>
2024-04-26 13:57:06 -0400

    Elliot Fahl - Checkpoint 2

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -1,7 +1,18 @@
+// Programmer: Elliot Fahl
+// Team Member: Ezinna Nwokenta
+// Bolt ID: 33-DF
+// Date: 4.24.2024
+// Program: Obstacle Course
+// Program URL: https://edu.sphero.com/program/16700422/edit
+
+// Elliot Fahl
 async function startProgram() {
 	setMainLed({ r: 0, g: 0, b: 255});
-	setSpeed(50);
+	await roll((getHeading() + 0), 60, 2	);
 	await delay (2);
-	setSpeed(0);
-	await roll((getHeading() + 90), 60, 1);
+	
+	//Elliot Fahl - Checkpoint 2
+	await roll((getHeading() - 35), 60, 2);
+	
+	
  }
