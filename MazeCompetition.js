a62b0f8d761fe76b8e556c70db8b7b08f86609ea
Fahl <2elliot.fahl@gulllakecs.org>
2024-04-30 13:28:44 -0400

    Added Beginning Comments & Start Program JS

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Elliot Fahl
+// Team Member: Ezinna Nwokenta
+// Bolt ID: 33-DF
+// Date: 4.30.2024
+// Program: Maze Competition
+// Program URL:
+
+async function startProgram() {
+	// Write code here
+}
