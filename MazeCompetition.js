353c01a899f638ac653e5e95cfd9da84a73c8685
Fahl <2elliot.fahl@gulllakecs.org>
2024-04-30 14:02:59 -0400

    Checkpoint Comments

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,5 +6,36 @@
 // Program URL:
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Elliot Fahl - Start Text & Audio Checkpoint Commit - Date:
+
+
+
+	// Programmer:             - Blue LED Checkpoint Commit - Date:
+
+
+
+	// Programmer: 			   - Sound One Checkpoint Commit - Date:
+
+
+
+	// Programmer:             - Red LED Checkpoint Commit - Date:
+
+
+
+	// Programmer: 			   - Sound Two Checkpoint Commit - Date:
+
+
+
+	// Programmer:             - Green LED Checkpoint Commit - Date:
+
+
+
+	// Programmer 			   - Purple LED & Sound Three Checkpoint Commit - Date:
+
+
+
+
+	// Programmer 			   - Finish Text & Audio Checkpoint Commit - Date:
+
+
 }
